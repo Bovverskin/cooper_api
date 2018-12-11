b41da5740822dd2bcb4edc741cdc8b0130c02d88
Jonathan <jon1969edwards@gmail.com>
2018-12-11 15:38:23 +0100

    Creates controller to update and retrieve users historical data, adds performance date model to store historical data and sessions spec for user authentication

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
+RSpec.describe PerformanceData, type: :model do
+    describe 'Database table' do
+        it { is_expected.to have_db_column "id" }
+        it { is_expected.to have_db_column "data" }
+    end
+
+    describe 'Relations' do
+        it { is_expected.to belong_to :user }
+    end
+end
+
 RSpec.describe User, type: :model do
     it 'should have valid Factory' do
         expect(create(:user)).to be_valid
